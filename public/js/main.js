fc1c1f4c6618abd6867bf9e4c339593e6026b7c9
patilav <patil.av@husky.neu.edu>
2015-04-12 18:05:26 -0400

    Updates to add the art work of different kinds

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,6 @@
 app.controller("NavController", function ($scope, LoginService, $location, $modal) {
     $scope.currentUser = null;
     $scope.color = "btn btn-success";
-
     $scope.signUp = function () {
         var modalInstance = $modal.open({
             templateUrl: 'partials/signup.html',
@@ -71,7 +70,7 @@ app.factory("LoginService", function ($http) {
             lastName: lastName,
             photo: photo
         };
-        $http.post("/signup", user)
+        $http.post("/api/signup", user)
         .success(function (response) {
             if (response.response == 'false')
                 callback(false);
@@ -81,7 +80,7 @@ app.factory("LoginService", function ($http) {
     }
 
     var login = function (user, callback) {
-        $http.post("/login", user)
+        $http.post("/api/login", user)
         .success(function (response) {
             currentUser = response[0];
             callback();
@@ -102,7 +101,7 @@ app.factory("LoginService", function ($http) {
         currentUser.firstName = firstName;
         currentUser.lastName = lastName;
 
-        $http.post("/updateprofile", currentUser)
+        $http.post("/api/updateprofile", currentUser)
         .success(function (response) {
             currentUser = response;
         });
@@ -119,201 +118,5 @@ app.factory("LoginService", function ($http) {
         updateCurrentUser: updateCurrentUser,
         updateCurrentUserPhoto: updateCurrentUserPhoto,
         signUp: signUp
-    }
-});
-
-app.controller("ProfileController", function ($scope, LoginService, $routeParams) {
-
-    var username = $routeParams.username;
-    $scope.username = username;
-    var currentUser = LoginService.getCurrentUser();
-
-    $scope.firstname_edit = currentUser.firstName;
-    $scope.lastname_edit = currentUser.lastName;
-    $scope.email_edit = currentUser.email;
-    $scope.password_edit = currentUser.password;
-    $scope.repassword_edit = currentUser.password;
-
-    if (currentUser.photo == "")
-        $scope.loc = "//placehold.it/100";
-    else
-        $scope.loc = currentUser.photo;
-
-    $scope.match = false;
-    $scope.save = false;
-
-    var handleFileSelect = function (evt) {
-        var files = evt.target.files;
-        var file = files[0];
-
-        if (files && file) {
-            var reader = new FileReader();
-
-            reader.onload = function (readerEvt) {
-                var binaryString = readerEvt.target.result;
-                $scope.loc = "data:image/jpeg;base64," + btoa(binaryString);
-                LoginService.updateCurrentUserPhoto($scope.loc);
-            };
-
-            reader.readAsBinaryString(file);
-        }
-    };
-
-    if (window.File && window.FileReader && window.FileList && window.Blob) {
-        document.getElementById('filePicker').addEventListener('change', handleFileSelect, false);
-    } else {
-        alert('The File APIs are not fully supported in this browser.');
-    }
-
-    $scope.saveChanges = function () {
-        var password = $scope.password_edit;
-        var repassword = $scope.repassword_edit;
-        var email = $scope.email_edit;
-        var firstName = $scope.firstname_edit;
-        var lastName = $scope.lastname_edit;
-
-        if (password != repassword) {
-            $scope.match = true;
-            $scope.save = false;
-        } else {
-            $scope.match = false;
-            $scope.save = true;
-            LoginService.updateCurrentUser(password, email, firstName, lastName);
-        }
-    }
-});
-
-app.controller("CommentController", function ($scope, $routeParams, $http) {
-    var username = $routeParams.username;
-
-    $http.get("/usercomment")
-    .success(function (response) {
-        $scope.comments = response;
-    });
-
-    $scope.add = function (text) {
-        var obj = { username: username, text: text };
-        $scope.text = null;
-        $http.post("/usercomment", obj)
-        .success(function (response) {
-            $scope.comments = response;
-        });
-    };
-});
-
-app.controller("ArtworkController", function ($scope, $routeParams, $http) {
-    var username = $routeParams.username;
-
-    $http.get("/userartwork")
-    .success(function (response) {
-        $scope.artwork = response;
-    });
-
-    $scope.add = function (art) {
-        var obj = { username: username, type: $scope.text, artwork: art };
-        $scope.type = null;
-        $scope.artwork = null;
-        $http.post("/userartwork", obj)
-        .success(function (response) {
-            $scope.artwork = response;
-        });
-    };
-});
-
-
-
-app.controller("SignupController", function ($scope, $modalInstance, LoginService, $http) {
-    $scope.loc = "//placehold.it/100";
-    $scope.fail = false;
-    var photo = "";
-
-    function callback(res) {
-        if (res == false) {
-            $scope.fail = true;
-            $scope.fail_msg = "Username already exists";
-        } else {
-            $modalInstance.dismiss('cancel');
-        }
-        
-    };
-
-    var handleFileSelect = function (evt) {
-        var files = evt.target.files;
-        var file = files[0];
-
-        if (files && file) {
-            var reader = new FileReader();
-
-            reader.onload = function (readerEvt) {
-                var binaryString = readerEvt.target.result;
-                $scope.loc = "data:image/jpeg;base64," + btoa(binaryString);
-                photo = $scope.loc;
-            };
-
-            reader.readAsBinaryString(file);
-        }
-    };
-
-    if (window.File && window.FileReader && window.FileList && window.Blob) {
-        $('#filePicker').on('change', handleFileSelect);
-    } else {
-        alert('The File APIs are not fully supported in this browser.');
-    }
-
-    $scope.ok = function () {
-
-        var username = $scope.username_edit;
-        var password = $scope.password_edit;
-        var repassword = $scope.repassword_edit;
-        var email = $scope.email_edit;
-        var firstName = $scope.firstname_edit;
-        var lastName = $scope.lastname_edit;
-
-        var emailFilter = /^[^@]+@[^@.]+\.[^@]*\w\w$/;
-        var illegalChars = /[\(\)\<\>\,\;\:\\\"\[\]]/;
-
-        if (firstName == null || firstName == "") {
-            $scope.fail = true;
-            $scope.fail_msg = "Enter First Name";
-        } else if (lastName == null || lastName == "") {
-            $scope.fail = true;
-            $scope.fail_msg = "Enter Last Name";
-        } else if (email == null || email == "" || !emailFilter.test(email) || email.match(illegalChars)) {
-            $scope.fail = true;
-            $scope.fail_msg = "Provide a valid email address";
-        } else if (username == null || username == "") {
-            $scope.fail = true;
-            $scope.fail_msg = "Enter UserName";
-        } else if (password != repassword) {
-            $scope.fail = true;
-            $scope.fail_msg = "Passwords must match";
-        } else if (password == "" || repassword == "" || password.length < 6 || !password.match(/[a-zA-Z]/) || !password.match(/[0-9]/)) {
-            $scope.fail = true;
-            $scope.fail_msg = "Password must be atleast 6 characters long with a number in it";
-        } else {
-            LoginService.signUp(username, password, email, firstName, lastName, photo, callback);
-        }
-    }
-
-    $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-    };
-
-});
-
-
-app.controller("HomeController", function ($scope, $routeParams, $http) {
-    $scope.myInterval = 5000;
-    var slides = $scope.slides = [];
-    $scope.addSlide = function () {
-        var newWidth = 600 + slides.length + 1;
-        slides.push({
-            image: 'http://placekitten.com/' + newWidth + '/300',
-            text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' +
-              ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-        });
-    };
-    for (var i = 0; i < 4; i++) {
-        $scope.addSlide();
     }
 });
