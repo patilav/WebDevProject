b36e41b52c9395f165f6699e4d1402047fcf2f90
patilav <patil.av@husky.neu.edu>
2015-04-17 19:55:14 -0400

    Add new image and fav artworks

---
+++ public/js/allprofile.js
@@ -0,0 +1,28 @@
+﻿app.controller("AllProfilesController", function ($scope, $routeParams, $http, $sce) {
+    var username = $routeParams.username;
+    $scope.username = username;
+    $http.get("/api/user")
+    .success(function (response) {
+        $scope.users = response;
+    });
+
+    $scope.followUser = function (index) {
+        $scope.selectedIndex = null;
+        $scope.selectedUser = null;
+        $scope.selectedIndex = index
+        $scope.selectedUser = $scope.users[index];
+        console.log("$scope.selectedIndex" + $scope.selectedIndex);
+        console.log("$scope.selectedUser" + $scope.selectedUser);
+        if (typeof $scope.selectedUser.followers == "undefined") {
+            $scope.selectedUser.followers = [];
+        }
+        var newFollwer = username;
+        if ($scope.selectedUser.followers.indexOf(newFollwer) == -1) {
+            $scope.selectedUser.followers.push(newFollwer);
+            $http.put("/api/user/" + $scope.selectedUser._id, $scope.selectedUser)
+            .success(function (response) {
+                $scope.users = response;
+            });
+        }
+    }
+});
---
+++ public/js/favartwork.js
@@ -0,0 +1,112 @@
+﻿app.controller("FavArtworkController", function ($scope, $routeParams, $http, $sce) {
+    $scope.selectedIndex = null;
+    $scope.selectedArtwork = null;
+    $scope.write = true;
+    $scope.commentText = null;
+
+    var username = $routeParams.username;
+
+    $scope.changetype = function () {
+        if ($scope.type == "Image") {
+            $scope.write = false;
+        } else {
+            $scope.write = true;
+        }
+    }
+
+    $scope.getLikeableArtwork = function (likes) {
+        var i;
+        for (i = 0; i < likes.length ; i++) {
+            if (i.username  == username) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    $scope.loc = "//placehold.it/100";
+    $scope.fail = false;
+    var photo = "";
+
+    var handleFileSelect = function (evt) {
+        var files = evt.target.files;
+        var file = files[0];
+
+        if (files && file) {
+            var reader = new FileReader();
+
+            reader.onload = function (readerEvt) {
+                var binaryString = readerEvt.target.result;
+                $scope.loc = "data:image/jpeg;base64," + btoa(binaryString);
+                photo = $scope.loc;
+            };
+
+            reader.readAsBinaryString(file);
+        }
+    };
+
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        $('#filePicker').on('change', handleFileSelect);
+    } else {
+        alert('The File APIs are not fully supported in this browser.');
+    }
+
+    populateData();
+
+    function populateData() {
+        $http.get("/api/userartwork")
+        .success(function (response) {
+            $scope.artwork = response;
+        });
+    }
+
+    function getUsername() {
+        return username;
+    }
+
+    $scope.showComment = function (index) {
+        $scope.selectedIndex = null;
+        $scope.selectedArtwork = null;
+        $scope.selectedIndex = index
+        $scope.selectedArtwork = $scope.artwork[index];
+        console.log("$scope.selectedIndex" + $scope.selectedIndex);
+        console.log("$scope.selectedArtwork" + $scope.selectedArtwork);
+    }
+
+
+    $scope.addComments = function (comment) {
+        if (typeof $scope.selectedArtwork.comments == "undefined") {
+            $scope.selectedArtwork.comments = [];
+        }
+        var newComment = {
+            text: comment.text,
+            username : username
+        }
+        $scope.selectedArtwork.comments.push(newComment);
+        $http.put("/api/userartwork/" + $scope.selectedArtwork._id + "/" + username, $scope.selectedArtwork)
+        .success(function (response) {
+            $scope.artwork = response;
+        });
+    }
+
+    $scope.upvote = function () {
+        if (typeof $scope.selectedArtwork.likes == "undefined") {
+            $scope.selectedArtwork.likes = [];
+        }
+        var newLike = {
+            count: 1,
+            username: username
+        }
+        $scope.selectedArtwork.likes.push(newLike);
+        $http.put("/api/userartwork/" + $scope.selectedArtwork._id + "/" + username, $scope.selectedArtwork)
+        .success(function (response) {
+            $scope.artwork = response;
+        });
+    }
+
+    $scope.trustSrc = function (src) {
+        return $sce.trustAsResourceUrl(src);
+    }
+
+});
